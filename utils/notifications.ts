d778ce1be8ffcdb599d1e6c2896d7b41ab1a07b3
Victor Crispin <victor.crispin@impact.com>
2026-02-11 06:57:22 -0500

    fix error

--- utils/notifications.ts
+++ utils/notifications.ts
@@ -115,7 +115,7 @@ export async function isSubscribedToPush(): Promise<boolean> {
 /**
  * Helper function to convert VAPID key
  */
-function urlBase64ToUint8Array(base64String: string): Uint8Array {
+function urlBase64ToUint8Array(base64String: string): Uint8Array<ArrayBuffer> {
   const padding = '='.repeat((4 - (base64String.length % 4)) % 4)
   const base64 = (base64String + padding)
     .replace(/-/g, '+')
@@ -128,7 +128,7 @@ function urlBase64ToUint8Array(base64String: string): Uint8Array {
     outputArray[i] = rawData.charCodeAt(i)
   }
 
-  return outputArray
+  return outputArray as Uint8Array<ArrayBuffer>
 }
 
 /**
